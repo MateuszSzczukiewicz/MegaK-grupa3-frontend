f43fd8ecfb56699a3f511700de8e431007661f43
Marta Brylska <tomica.marta@gmail.com>
2023-11-22 22:44:09 +0100

    Check test endpoints GET /test/:id and POST /test

---
+++ src/api/testAPI.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export async function getTest(id): Promise<{id: string, name: string}> {
+    try {
+        const res = await axios.get(`http://localhost:3000/test/${id}`);
+        return res.data;
+    } catch (error) {
+        return error;
+    }
+}
+
+export async function addTest(name): Promise<{id: string, name: string}> {
+    try {
+        const res = await axios.post('http://localhost:3000/test/', {
+            name,
+        });
+        return res.data;
+    } catch (error) {
+        return error;
+    }
+}
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,5 +1,22 @@
 import { Header } from '../components/atoms/Header/Header.tsx';
+import {useEffect} from "react";
+import {addTest, getTest} from "../api/testAPI";
 
 export const Homepage = () => {
+
+	useEffect(() => {
+		(async () => {
+			const data = await getTest('123');
+			console.log(data);
+		})()
+	}, [])
+
+	useEffect(() => {
+		(async () => {
+			const data = await addTest('testowy');
+			console.log(data);
+		})()
+	}, [])
+
 	return <Header />;
 };

6a401a796ec41c24f8ed026ac371edcfd4bd9099
Marta Brylska <tomica.marta@gmail.com>
2023-11-25 01:56:11 +0100

    Add go back element

--- src/components/organisms/StudentPrimaryInfo/StudentPrimaryInfo.styles.ts
+++ src/components/organisms/StudentPrimaryInfo/StudentPrimaryInfo.styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const StudentPrimaryInfoSection = styled.section`
+	position: relative;
 	display: flex;
 	justify-content: center;
 	align-items: center;

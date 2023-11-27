fd525a9a59d34cf41fcabbdcc18913176d2ef74e
FranciszekKaniewski <fkaniewski.web.com>
2023-11-27 15:15:34 +0100

    StudentHeader and StudentRowInfo styles changed

--- src/components/atoms/StudentRowInfo/StudentRowInfo.style.ts
+++ src/components/atoms/StudentRowInfo/StudentRowInfo.style.ts
@@ -2,19 +2,30 @@ import styled from "styled-components";
 
 export const StudentRowInfoStyle = styled.div`
     display: flex;
+    flex-wrap: wrap;
+    justify-content: start;
+    margin: 0;
+    width: 100%;
+    ${({ theme }) => theme.mq.sm}{
+      width: 400px;
+      margin: 10px 0 0 25px;
+    }
 `
 export const ImgStyle = styled.img`
     float: left;
     width: 50px;
     height: 50px;
-    margin: 0 20px 0 60px;
+    margin-left: 30px;
     border-radius: 100%;
 `
 export const BStyle = styled.p`
     color: ${({ theme }) => theme.color.white};
 `
 export const DateStyle = styled.p`
-    float: left;
+    margin-right: 0;
+    ${({ theme }) => theme.mq.sm}{
+      margin-right: 40px;
+    }
 `
 export const NameStyle = styled.span`
     line-height: 60px;

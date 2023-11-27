f4afbec55a8d38a0334c66cd5a8e815b360d07f6
FranciszekKaniewski <fkaniewski.web.com>
2023-11-27 15:17:27 +0100

    StudentsPageMain changed

--- src/components/molecules/StudentsPageMain/StudentsPageMain.tsx
+++ src/components/molecules/StudentsPageMain/StudentsPageMain.tsx
@@ -1,24 +1,61 @@
-import {StudentsMainStyle, HrStyle} from './StudentsPageMain.style'
+import {StudentsMainStyle, TopStyle, HrStyle} from './StudentsPageMain.style'
 import {StudentRow} from "../StudentRow/StudentRow";
 import {SearchBar} from "../../atoms/SearchBar/SearchBar";
 import {FilterBtn} from "../../atoms/FilterBtn/FilterBtn";
+import {useEffect, useState} from "react";
+import {StudentsPageFooter} from "../StudentsPageFooter/StudentsPageFooter";
 
 type Props ={
     simplified:boolean;
+    studentsList:any;
 }
 
-export const StudentsPageMain = ({simplified}:Props) =>{
+export const StudentsPageMain = ({simplified,studentsList}:Props) =>{
+
+    const [rowsLimit, setRowsLimit] = useState<number>(5);
+    const [activePage, setActivePage] = useState<number>(0);
+    const [searchBarValue, setSearchBarValue] = useState<string>('');
+
+    const [studentsRows, setStudentsRows] = useState(studentsList);
+
+    useEffect(() => {
+        setStudentsRows(
+            studentsList.filter(student => `${student.name} ${student.surname}`.includes(searchBarValue)
+        ));
+    }, [searchBarValue]);
+
+
+    const renderRows = studentsRows.map( (student,i) => {
+        const condition = (i+1 > activePage * rowsLimit && i < (activePage+1) * rowsLimit);
+
+        return (condition) ? <StudentRow
+            key={student.id}
+            simplified={simplified}
+            name={student.name}
+            surname={student.surname}
+            reservationDate={student.reservationDate}
+        /> : null
+    })
 
     return(
+        <>
         <StudentsMainStyle>
-            <SearchBar/>
-            <FilterBtn/>
+            <TopStyle>
+                <SearchBar setSearchBarValue={setSearchBarValue}/>
+                <FilterBtn/>
+            </TopStyle>
 
             <HrStyle/>
-            <StudentRow simplified={simplified}/>
-            <StudentRow simplified={simplified}/>
-            <StudentRow simplified={simplified}/>
-            {/* Students.map() ...*/}
+            {renderRows}
         </StudentsMainStyle>
+
+        <StudentsPageFooter
+            rowsLimit={rowsLimit}
+            setRowsLimit={setRowsLimit}
+            LengthOfStudentsList={studentsRows.length}
+            activePage={activePage}
+            setActivePage={setActivePage}
+        />
+        </>
     )
 }

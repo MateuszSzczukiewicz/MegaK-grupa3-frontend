a8597d4970e913027c6589cc736683c4d1114afb
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2023-11-28 11:22:16 +0100

    Edit star icon

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Root } from './pages/Root.tsx';
 import { AppProviders } from './providers/AppProvider.tsx';
-import './assets/styles/fonts.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<StrictMode>

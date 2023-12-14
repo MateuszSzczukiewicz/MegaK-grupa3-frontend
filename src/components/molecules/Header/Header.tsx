0bb04fbe95904227aae617567f6d2e3910a4a521
Marta Brylska <tomica.marta@gmail.com>
2023-12-15 00:28:38 +0100

    Change routes for header logo click.

--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -2,12 +2,26 @@ import { useNavigate } from 'react-router-dom';
 import { StyledHeader, StyledWrapper, StyledLogo } from './Header.styles.ts';
 import Logo from '../../../assets/images/logo.webp';
 import { UserElement } from '../../atoms/UserElement/UserElement';
+import { useContext } from 'react';
+import { UserContext } from '../../../contexts/user.context';
 
 export const Header = () => {
 	const navigate = useNavigate();
+	const { user } = useContext(UserContext);
+	const { userRole } = user;
 
 	const handleClick = () => {
-		navigate('/');
+		switch (userRole) {
+			case 0:
+				navigate('/admin');
+				break;
+			case 1:
+				navigate('/student');
+				break;
+			case 2:
+				navigate('/students');
+				break;
+		}
 	};
 
 	return (
